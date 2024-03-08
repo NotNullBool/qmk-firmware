d6d8959a3b1ee71689a9b071f21e66e85c14f5cd
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2024-03-08 18:24:59 -0500

    qmk: nav layer creation

--- keyboards/keychron/c3_pro/ansi/red/keymaps/NotNullBool/keymap.c
+++ keyboards/keychron/c3_pro/ansi/red/keymaps/NotNullBool/keymap.c
@@ -30,6 +30,7 @@ enum layers{
     BASE_QWERTY,
     BASE_GAMING,
     BASE_PROGRAMMING,
+    NAV,
     FN,
 };
 
@@ -68,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,                        KC_F1,        KC_F2,        KC_F3,        KC_F4,    KC_F5,    KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,   KC_F11,      KC_F12,                KC_PSCR,  KC_SCRL,  KC_PAUS,
         KC_GRV,          KC_1,         KC_2,         KC_3,         KC_4,         KC_5,     KC_6,     KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,  KC_EQL,      KC_BSPC,               KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,          KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,     KC_J,     KC_L,         KC_U,         KC_Y,         KC_SCLN,      KC_LBRC,  KC_RBRC,     KC_BSLS,               KC_DEL,   KC_END,   KC_PGDN,
-        _______,   LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,     KC_M,     RCTL_T(KC_N), RSFT_T(KC_E), LALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,               RSFT_T(KC_ENT),
+        MO(NAV),   LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,     KC_M,     RCTL_T(KC_N), RSFT_T(KC_E), LALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,               RSFT_T(KC_ENT),
         KC_LSFT,                       KC_X,         KC_C,         KC_D,         KC_V,     KC_Z,     KC_K,         KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,               KC_RSFT,                         KC_UP,
-        KC_LCTL,         KC_LWIN,      KC_LALT,                                            LT(BASE_PROGRAMMING,KC_SPC),                                             KC_RALT,      MO(FN),   KC_LWIN,     KC_RCTL,               KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,         KC_LWIN,      KC_LALT,                                            LT(BASE_PROGRAMMING,KC_SPC),                         KC_RALT,      MO(FN),   KC_LWIN,     KC_RCTL,              KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [BASE_QWERTY] = LAYOUT_tkl_ansi(
         _______,             _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  _______,
@@ -96,6 +97,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   LSFT_T(KC_DOT),   LSFT_T(KC_MINS),  LSFT_T(KC_PPLS),  LSFT_T(KC_PAST),  LSFT_T(KC_GRV),  LSFT_T(KC_SLSH),  LSFT_T(KC_QUOT),  LSFT_T(KC_COMM),     LSFT_T(KC_DOT),    LSFT_T(KC_SLSH),                       _______,            _______,
         _______,   _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______
     ),
+
+    [NAV] = LAYOUT_tkl_ansi(
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
+        _______,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
+        _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
 
     [FN] = LAYOUT_tkl_ansi(
         _______,            KC_BRID,  KC_BRIU,  _______,  _______,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  DF(BASE_GAMING),  DF(BASE_QWERTY),  DF(BASE),
